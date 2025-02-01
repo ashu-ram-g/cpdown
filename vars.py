1370d75dbf54b372cea68a4a8fc2ee5285bd68eb
ashu-ram-g <jairam40101@gmail.com>
2025-02-01 16:31:16 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,6 +1,6 @@
 api_id = 18116881 #change_with_your_Api_id.
 api_hash = "cca3bacf40fb3ebcb4f075b2e46ff1bd" #change_with_your_Api_hash.
-bot_token = "7861730684:AAFXz35XKHxY5Jf3YnCZ6fvl6GmCngk3bmw" #change_with_your_bot_token.
+bot_token = "7428927891:AAHoNzLYya8aRaGF2y-ngluSxzy29LlN3XI" #change_with_your_bot_token.
 auth_users = [1445673621] #change_with_your_user_id.
 sudo_user = 1445673621 #change_with_your_user_id.
 log_channel = -1002000904124 #change_with_your_channel_id.
